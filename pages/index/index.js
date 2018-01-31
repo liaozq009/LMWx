a3548c4e35c92e6c841213786e1549005d7954b7
liaozq009 <245822113@qq.com>
2018-01-31 17:33:53 +0800

    日期

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,32 @@
 //index.js
+// 日期选择
+var formatDate = function (ymd) { //日期格式化
+  return ymd.replace(/(\d{4})\-(\d{1,2})\-(\d{1,2})/g, function (ymdFormatDate, y, m, d) {
+    m < 10 && (m = '0' + m);
+    d < 10 && (d = '0' + d);
+    return y + '-' + m + '-' + d;
+  });
+};
+var today = new Date();
+var startTimeStr = new Date(today.getTime() + 86400000 * 0);
+var startTime = formatDate(startTimeStr.getFullYear() + '-' + (startTimeStr.getMonth() + 1) + '-' + startTimeStr.getDate());
+var endTimeStr = new Date(today.getTime() + 86400000 * 3);
+var endTime = formatDate(endTimeStr.getFullYear() + '-' + (endTimeStr.getMonth() + 1) + '-' + (endTimeStr.getDate()));
+
 Page({
   data: {
     activeOw:true,
     activeRt: false,
+    fromCityVal:'广州',
+    toCityVal:'北京',
+    fromCityArray: ['广州', '北京', '上海', '深圳'],
+    toCityArray: ['广州', '北京', '上海', '深圳'],
+    startDate: startTime,
+    startMinDate: startTime,
+    startMaxDate:'',
+    endDate: endTime,
+    endMinDate: startTime,
+    endMaxDate: '',
   },
   onLoad: function () {
     
@@ -20,5 +44,40 @@ Page({
         activeRt: true
       })
     }
+  },
+  selectFromCity:function(e){
+    if (this.data.toCityVal == this.data.fromCityArray[e.detail.value]) {
+      wx.showModal({
+        title: '提示',
+        content: '出发地和目的地不能一样',
+      })
+    } else {
+      this.setData({
+        fromCityVal: this.data.fromCityArray[e.detail.value]
+      })
+    }
+  },
+  selectToCity: function (e) {
+    if (this.data.fromCityVal == this.data.toCityArray[e.detail.value]){
+      wx.showModal({
+        title: '提示',
+        content: '出发地和目的地不能一样',
+      })
+    }else{
+      this.setData({
+        toCityVal: this.data.toCityArray[e.detail.value]
+      })
+    }
+  },
+  changeStartDate:function(e){
+    
+    this.setData({
+      startDate: e.detail.value
+    })
+  },
+  changeEndDate:function(e){
+    this.setData({
+      endDate: e.detail.value
+    })
   },
 })
